a4d71ad77fd8a809f7176d709521d93fcf5a2492
68681395 <happycoder@foxmail.com>
2020-11-13 16:34:21 +0800

    Just auto-commit for synchronizing workspace

---
+++ pack.bat
@@ -0,0 +1,3 @@
+for /d %%X in (*) do "c:\Program Files\7-Zip\7z.exe" a -ttar "%%X.tar" "%%X\"
+for %%X in (*.tar) do ("c:\Program Files\7-Zip\7z.exe" a -tgzip "%%X.gz" "%%X" )
+for %%X in (*.tar) do (del "%%X")
