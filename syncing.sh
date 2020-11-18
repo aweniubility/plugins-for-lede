7173252039ff886b859d9cb8ddd0c14d18a23f9d
68681395 <happycoder@foxmail.com>
2020-11-18 11:50:53 +0800

    0

--- syncing.sh
+++ syncing.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read  -p 'commit message>'  msg
- if [ -z "$msg" ]; then
+read  -p 'commit message>'  
+ if [ -z "$?" ]; then
      echo "committing as comment : 'auto-commit for synchronizing'"
-	 msg="auto-commit for synchronizing"
+	 ?="auto-commit for synchronizing"
  fi
 git pull
 git add *
 git add -A
-git commit -m "$msg"
+git commit -m "$?"
 git push -u origin master
  
  
