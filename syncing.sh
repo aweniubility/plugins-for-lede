4c04e30d86dee7f73af83d6e03f6cf54ade479ec
68681395 <happycoder@foxmail.com>
2020-11-18 14:37:41 +0800

    Synchronizing

--- syncing.sh
+++ syncing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-read  -p 'Commit message>' msg
- if [ -z "$msg" ]; then     
+if [$1 ='auto']; then
+	msg="Synchronizing"
+else
+	read  -p 'Commit message>' msg
+	if [ -z "$msg" ]; then     
 	 msg="Synchronizing"
- fi
- 
-echo "Committing as '$msg'"
+	 echo "Committing as '$msg'"
+	fi  
+fi
 git pull
 git add *
 git add -A
 git commit -m "$msg"
 git push -u origin master
- 
- 
